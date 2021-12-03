5a8d5e21a61463ff8f38332b02f9e55f65083efa
LyallInsanity <addyvv@hotmail.nl>
2021-12-03 15:22:12 +0000

    remove unused var

--- src/PokemonCard.tsx
+++ src/PokemonCard.tsx
@@ -8,14 +8,12 @@ const PokemonCard: FC<{name: string, url: string}> = (props) =>{
     const name = props.name
     const url  = props.url
     const pokeIndex  = url.split("/")[url.split("/").length - 2]
-    const [pokemonDetails, setPokemonDetails] = useState<[IPokemonDetails] | null>(null)
     const [pokemonType, setPokemonType] = useState('type')
     const [imageUrl, setImageUrl] = useState('')
 
   useEffect(()=>{
       let cancel: Canceler
       axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeIndex}`, {cancelToken: new axios.CancelToken(c => cancel =c )}).then(res =>{
-        setPokemonDetails([res.data])
         setImageUrl(res.data.sprites.front_default)
         setPokemonType(res.data.types[0].type.name)
         console.log(res.data)
